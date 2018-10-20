1064e6afc9bf85850a3032cc58187c89203249a2
Toshiki Baba <yz.t0.r33@gmail.com>
2018-10-20 22:02:54 +0900

    todolist

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,10 +1,41 @@
 <template lang="pug">
 section
+  form(@submit.prevent="sendTodo")
+    input(v-model="todo")
+    input(type="submit" val="追加")
+
+  ul(v-for="item in list")
+    li(@click="doneTodo(item)"): span status: {{ item.status }}
+    s(v-if="item.status =='done'") {{ item.text }}
+    span(v-else) {{ item.text }}
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  data: function() {
+    return {
+      todo: "",
+      list: []
+    }
+  },
+  methods: {
+    sendTodo: function() {
+      this.list.push({
+        text: this.todo,
+        status: 'todo'
+      });
+      this.todo = "";
+    },
+    doneTodo: function(item) {
+      if (item.status == "done") {
+        item.status = "todo";
+      } else {
+        item.status = "done";
+      }
+
+    }
+  }
 };
 </script>
 
